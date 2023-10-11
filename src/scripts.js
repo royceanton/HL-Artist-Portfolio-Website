9a009388b0f4525924f7d54295abd6a18816804b
Royce Anton Jose <106367095+royceanton@users.noreply.github.com>
2023-10-11 06:02:08 +0000

    nav bar implementation

---
+++ src/scripts.js
@@ -0,0 +1,12 @@
+console.log("Script loaded");
+
+document.querySelector(".hamburger-menu").addEventListener("click", function () {
+    console.log("Hamburger menu clicked");
+    const dropdown = document.querySelector(".menu-dropdown");
+    dropdown.classList.toggle("open");
+});
+
+document.querySelector(".close-menu").addEventListener("click", function () {
+    const dropdown = document.querySelector(".menu-dropdown");
+    dropdown.classList.remove("open");
+});
