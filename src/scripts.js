39b68c5d7be4e6458145fd146c99c1458f9a57a5
Royce Anton Jose <106367095+royceanton@users.noreply.github.com>
2023-10-12 11:34:27 +0000

    fixed the enlarged gallery with tap close and close button

--- src/scripts.js
+++ src/scripts.js
@@ -12,3 +12,36 @@ document.querySelector(".close-menu").addEventListener("click", function () {
   const dropdown = document.querySelector(".menu-dropdown");
   dropdown.classList.remove("open");
 });
+
+
+// Function to show overlay with the selected image
+function showOverlay(imgSrc, altText) {
+  const overlayImage = document.querySelector('.overlay img');
+  overlayImage.src = imgSrc;
+  overlayImage.alt = altText;
+  document.querySelector('.overlay').classList.add('active');
+}
+
+// Event listeners for each image in the gallery
+const galleryImages = document.querySelectorAll('.artwork-gallery img');
+galleryImages.forEach(img => {
+  img.addEventListener('click', function() {
+      showOverlay(img.src, img.alt);
+  });
+});
+
+// Event listener for the overlay to close it
+document.querySelector('.overlay').addEventListener('click', function() {
+  document.querySelector('.overlay').classList.remove('active');
+});
+
+// To prevent the overlay from closing when the image itself or the navigation buttons are clicked
+document.querySelector('.overlay img').addEventListener('click', function(event) {
+  event.stopPropagation();
+});
+document.querySelector('.overlay .prev').addEventListener('click', function(event) {
+  event.stopPropagation();
+});
+document.querySelector('.overlay .next').addEventListener('click', function(event) {
+  event.stopPropagation();
+});
