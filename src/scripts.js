4f8226663890d55a3e16cfff34372de297522741
Royce Anton Jose <106367095+royceanton@users.noreply.github.com>
2023-10-12 08:08:47 +0000

    new file:   src/assets/gallery/pic-1.png 	new file:   src/assets/gallery/pic-2.png 	new file:   src/assets/gallery/pic-3.png 	new file:   src/assets/gallery/pic-4.png 	new file:   src/assets/gallery/pic-5.png 	new file:   src/assets/gallery/pic-6.png 	modified:   src/index.html 	modified:   src/scripts.js 	modified:   src/styles.css

--- src/scripts.js
+++ src/scripts.js
@@ -1,12 +1,14 @@
 console.log("Script loaded");
 
-document.querySelector(".hamburger-menu").addEventListener("click", function () {
+document
+  .querySelector(".hamburger-menu")
+  .addEventListener("click", function () {
     console.log("Hamburger menu clicked");
     const dropdown = document.querySelector(".menu-dropdown");
     dropdown.classList.toggle("open");
-});
+  });
 
 document.querySelector(".close-menu").addEventListener("click", function () {
-    const dropdown = document.querySelector(".menu-dropdown");
-    dropdown.classList.remove("open");
+  const dropdown = document.querySelector(".menu-dropdown");
+  dropdown.classList.remove("open");
 });
