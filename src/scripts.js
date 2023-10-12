1d730789d65481e030ea0cb8a992b366be32e69b
Royce Anton Jose <106367095+royceanton@users.noreply.github.com>
2023-10-12 14:31:49 +0000

    transport test to index files

--- src/scripts.js
+++ src/scripts.js
@@ -1,80 +1,65 @@
-console.log("Script loaded");
+// Hamburger menu functionality
+document
+  .querySelector(".hamburger-menu")
+  .addEventListener("click", function () {
+    document.querySelector(".menu-dropdown").classList.add("open");
+  });
 
-// Get all dropdown links
+// Close button functionality
+document.querySelector(".close-menu").addEventListener("click", function () {
+  document.querySelector(".menu-dropdown").classList.remove("open");
+});
+
+// Dropdown link click script
 const dropdownLinks = document.querySelectorAll(".menu-dropdown a");
 
-// Add event listener to each link
 dropdownLinks.forEach((link) => {
   link.addEventListener("click", function (event) {
     // Prevent default behavior of the link
     event.preventDefault();
 
-    // Close the dropdown
-    const dropdown = document.querySelector(".menu-dropdown");
-    dropdown.classList.remove("open");
-
     // Scroll to the target section
     const targetSection = document.querySelector(link.getAttribute("href"));
     if (targetSection) {
-      const topPosition =
-        targetSection.offsetTop -
-        document.querySelector(".header").offsetHeight;
+      const topPosition = targetSection.offsetTop;
       window.scrollTo({
         top: topPosition,
         behavior: "smooth",
       });
+
+      // Close the dropdown after scrolling
+      document.querySelector(".menu-dropdown").classList.remove("open");
     }
   });
 });
 
-document
-  .querySelector(".hamburger-menu")
-  .addEventListener("click", function () {
-    console.log("Hamburger menu clicked");
-    const dropdown = document.querySelector(".menu-dropdown");
-    dropdown.classList.toggle("open");
-  });
+// Event listeners for enlarging artwork images
+const artworkImages = document.querySelectorAll(".artwork-display img");
 
-document.querySelector(".close-menu").addEventListener("click", function () {
-  const dropdown = document.querySelector(".menu-dropdown");
-  dropdown.classList.remove("open");
-});
-
-// Function to show overlay with the selected image
-function showOverlay(imgSrc, altText) {
-  const overlayImage = document.querySelector(".overlay img");
-  overlayImage.src = imgSrc;
-  overlayImage.alt = altText;
-  document.querySelector(".overlay").classList.add("active");
-}
-
-// Event listeners for each image in the gallery
-const galleryImages = document.querySelectorAll(".artwork-gallery img");
-galleryImages.forEach((img) => {
+artworkImages.forEach((img) => {
   img.addEventListener("click", function () {
-    showOverlay(img.src, img.alt);
+    const overlay = document.querySelector(".overlay");
+    const overlayImg = overlay.querySelector("img");
+
+    // Set the src of the overlay image to the clicked image's src
+    overlayImg.src = img.src;
+
+    // Show the overlay
+    overlay.classList.add("active");
   });
 });
 
-// Event listener for the overlay to close it
-document.querySelector(".overlay").addEventListener("click", function () {
-  document.querySelector(".overlay").classList.remove("active");
+// Event listener for the close button in the image overlay
+document
+  .querySelector(".close-menu-overlay")
+  .addEventListener("click", function () {
+    document.querySelector(".overlay").classList.remove("active");
+  });
+
+// Optional: Hide the overlay if anywhere outside the image is clicked
+document.querySelector(".overlay").addEventListener("click", function (event) {
+  // Check if the clicked target is the overlay itself and not an inner element
+  if (event.target === event.currentTarget) {
+    document.querySelector(".overlay").classList.remove("active");
+  }
 });
-
-// To prevent the overlay from closing when the image itself or the navigation buttons are clicked
-document
-  .querySelector(".overlay img")
-  .addEventListener("click", function (event) {
-    event.stopPropagation();
-  });
-document
-  .querySelector(".overlay .prev")
-  .addEventListener("click", function (event) {
-    event.stopPropagation();
-  });
-document
-  .querySelector(".overlay .next")
-  .addEventListener("click", function (event) {
-    event.stopPropagation();
-  });
-
